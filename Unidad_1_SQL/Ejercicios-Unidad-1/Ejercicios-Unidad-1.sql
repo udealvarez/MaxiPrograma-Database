770f1c465ce7bb6d472e3111833c11f7d91216a5
Eduardo AlvareZ <udealvarez@icloud.com>
2025-11-23 00:21:16 -0300

    Ejercicio 3 | Unidad 1

--- Unidad_1_SQL/Ejercicios-Unidad-1/Ejercicios-Unidad-1.sql
+++ Unidad_1_SQL/Ejercicios-Unidad-1/Ejercicios-Unidad-1.sql
@@ -10,3 +10,12 @@ SELECT R.Nombre, R.Apellido, R.FechaNacimiento, P.Nombre
 FROM Reparto R
 LEFT JOIN Paises P ON R.IdNacionalidad = P.Id
 WHERE YEAR(FechaNacimiento) BETWEEN 1970 AND 1979
+
+
+-- 3) Obtener las Peliculas que se encuentran en la Plataforma de Disney+.
+SELECT * FROM Plataformas --analizo el Id de Disney+  -> id = 3
+
+SELECT P.Titulo AS Pelicula FROM Peliculas P
+INNER JOIN [Peliculas.Plataformas] PP ON P.Id = PP.IdPelicula
+INNER JOIN Plataformas PL ON PP.IdPlataforma = PL.Id
+WHERE  PL.Id = 3
