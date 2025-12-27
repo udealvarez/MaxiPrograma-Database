15e0b399ef767bc644b05ab3ff29620beaaa7fa4
Eduardo AlvareZ <udealvarez@icloud.com>
2025-12-27 18:24:33 -0300

    Instruccion DROP TABLE

---
+++ Unidad_2_Construccion_DB/07-Drop-Table/Drop Table.sql
@@ -0,0 +1,12 @@
+-- DROP -> elimina la tabla y sus registros 
+
+DROP TABLE Entrenadores -- -> borro la tabla ENTRENADORES
+
+
+-- DROP de una BASE DE DATOS
+DROP DATABASE MundoPokemon_DB2
+
+
+-- DROP UNA COLUMNA
+ALTER TABLE Pokemons
+	DROP COLUMN Altura;
